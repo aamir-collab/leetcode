c4848d552b9353b6296128331d9563a0583b94c6
hikikomoriii <100182441+hikikomoriii@users.noreply.github.com>
2022-06-13 11:44:13 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (28.23%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,7 +1,29 @@
 class Solution {
 public:
     int removePalindromeSub(string s) {
-        if(equal(s.begin(), s.begin() + s.size()/2, s.rbegin())) return 1;
-        return 2;
+        if(s == " "){
+            return 0;
+        }
+        int ans = 0;
+        
+        if(isPalin(s)){
+            ans = 1;
+        }
+        else{
+            ans = 2;
+        }
+        return ans;
+    }
+    bool isPalin(string s){
+        int l = 0;
+        int r = s.length()-1;
+        while(l<=r){
+            if(s[l] != s[r]){
+             return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
     }
 };
