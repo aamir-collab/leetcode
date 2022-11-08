7f5fef96f741970a21d207f2c4df2cf8e0f19ab9
Aamir <100182441+hikikomoriii@users.noreply.github.com>
2022-11-08 12:03:30 +0530

    Time: 3 ms (82.74%), Space: 6.5 MB (54.48%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string ans = "";
+        stack<char>st;
+        for(int i = 0; i<s.length(); i++){
+            if(st.empty()){
+                st.push(s[i]);
+            }
+            else{
+                if(abs(st.top() - s[i]) == 32){
+                    st.pop();
+                }
+                else{
+                    st.push(s[i]);
+                }
+            }
+        }
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
